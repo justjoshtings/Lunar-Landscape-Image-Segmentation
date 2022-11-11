678cde8a1c7d4512cb3bff9e23356d67cb3e245e
Saharae <saharae27@gmail.com>
2022-11-11 16:46:12 -0500

    updates to EDA and moving to main code folder

---
+++ Code/EDA.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#getting paths
+CODE_PATH = os.getcwd()
+os.chdir('..')
+BASE_PATH = os.getcwd()
+os.chdir(CODE_PATH)
+DATA_PATH = os.path.join(BASE_PATH, 'Data')
+
+bounding = pd.read_csv(os.path.join(DATA_PATH, 'bounding_boxes.csv'))
+imgs = os.listdir(os.path.join(DATA_PATH, 'images', 'clean'))
+
+dat = []
+
+#[R, G, B], black = [0,0,0]
+if os.path.exists(os.path.join(DATA_PATH,'images_summary.csv')):
+    print('already image collected data')
+else:
+    print('doing image stuff')
+    for img_path in imgs:
+        img = plt.imread(os.path.join(DATA_PATH, 'images', 'clean', img_path))
+        sums = np.sum(img, axis = 2)
+        idx = np.argmax(img, axis = 2)
+        blues = len(np.where(idx == 2)[0])
+        greens = len(np.where(idx == 1)[0])
+        reds = len(np.where(np.logical_and(idx==0, sums>0))[0])
+        blacks = len(np.where(np.logical_and(idx==0, sums==0))[0])
+        dat.append([img_path, reds, greens, blues, blacks])
+
+    df = pd.DataFrame(dat, columns = ['image', 'reds', 'greens', 'blues', 'blacks'])
+    df.to_csv(os.path.join(DATA_PATH,'images_summary.csv'))
+
+
+print('making heatmaps')
+red_mat = np.zeros((480,720))
+green_mat = np.zeros((480,720))
+blue_mat = np.zeros((480,720))
+black_mat = np.zeros((480,720))
+
+for img_path in imgs:
+    img = plt.imread(os.path.join(DATA_PATH, 'images', 'clean', img_path))
+    sums = np.sum(img, axis = 2)
+    idx = np.argmax(img, axis = 2)
+    blues = np.where(idx == 2)
+    greens = np.where(idx == 1)
+    reds = np.where(np.logical_and(idx==0, sums>0))
+    blacks = np.where(np.logical_and(idx==0, sums==0))
+    red_mat[reds]+=1
+    green_mat[greens]+=1
+    blue_mat[blues]+=1
+    black_mat[blacks]+=1
+
+np.save(os.path.join(DATA_PATH,'blue_dat.npy'), blue_mat)
+np.save(os.path.join(DATA_PATH,'red_dat.npy'), red_mat)
+np.save(os.path.join(DATA_PATH,'green_dat.npy'), green_mat)
+np.save(os.path.join(DATA_PATH,'black_dat.npy'), black_mat)
--- sahara-ensley-individual-project/Code/EDA.py
+++ sahara-ensley-individual-project/Code/EDA.py
@@ -1,36 +1,22 @@
-import kaggle
-from kaggle.api.kaggle_api_extended import KaggleApi
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-os.environ['KAGGLE_USERNAME'] = "saharaensley"
-os.environ['KAGGLE_KEY'] = "14b76f6d948ee90f7856dec349c37c6f"
 #getting paths
 CODE_PATH = os.getcwd()
 os.chdir('..')
 BASE_PATH = os.getcwd()
 os.chdir(CODE_PATH)
 DATA_PATH = os.path.join(BASE_PATH, 'Data')
-#DATA_PATH = '/Users/sahara/Documents/GW/ML2/Final-Project-GroupX/sahara-ensley-individual-project/Data' # for testing
-
-#downloading data
-if not os.path.exists(DATA_PATH):
-    api = KaggleApi()
-    api.authenticate()
-    api.dataset_download_files('romainpessia/artificial-lunar-rocky-landscape-dataset', path=DATA_PATH, unzip=True)
-    print('done')
-else:
-    print("data already exists")
 
 bounding = pd.read_csv(os.path.join(DATA_PATH, 'bounding_boxes.csv'))
 imgs = os.listdir(os.path.join(DATA_PATH, 'images', 'clean'))
 
 dat = []
 #[R, G, B], black = [0,0,0]
-if os.path.exists('images_summary.csv'):
-    print('alraedy collected data')
+if os.path.exists(os.path.join(DATA_PATH,'images_summary.csv')):
+    print('already collected data')
 else:
     print('doing image stuff')
     for img_path in imgs:
@@ -44,6 +30,30 @@ else:
         dat.append([img_path, reds, greens, blues, blacks])
 
     df = pd.DataFrame(dat, columns = ['image', 'reds', 'greens', 'blues', 'blacks'])
-    df.to_csv('images_summary.csv')
+    df.to_csv(os.path.join(DATA_PATH,'images_summary.csv'))
 
-df = pd.read_csv("images_summary.csv")
+make_heatmaps = True
+if make_heatmaps:
+    red_mat = np.zeros((480,720))
+    green_mat = np.zeros((480,720))
+    blue_mat = np.zeros((480,720))
+    black_mat = np.zeros((480,720))
+
+    for img_path in imgs:
+        img = plt.imread(os.path.join(DATA_PATH, 'images', 'clean', img_path))
+        sums = np.sum(img, axis = 2)
+        idx = np.argmax(img, axis = 2)
+        blues = np.where(idx == 2)
+        greens = np.where(idx == 1)
+        reds = np.where(np.logical_and(idx==0, sums>0))
+        blacks = np.where(np.logical_and(idx==0, sums==0))
+        red_mat[reds]+=1
+        green_mat[greens]+=1
+        blue_mat[blues]+=1
+        black_mat[blacks]+=1
+
+    np.save(os.path.join(DATA_PATH,'blue_dat.npy'), blue_mat)
+    np.save(os.path.join(DATA_PATH,'red_dat.npy'), red_mat)
+    np.save(os.path.join(DATA_PATH,'green_dat.npy'), green_mat)
+    np.save(os.path.join(DATA_PATH,'black_dat.npy'), black_mat)
+
